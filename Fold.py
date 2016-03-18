c1d8f9f0d140adf357ee2266b87cea1a7b65ce53
ShinHyeok <maximum@unist.ac.kr>
2016-03-18 13:27:34 +0900

    Update Fold.py

--- Fold.py
+++ Fold.py
@@ -1,12 +1,16 @@
 # X.trop-Tail
-f= open ('fit.txt','r')
+f= open ('mydata.txt','r')
 g= open ('fold.txt','w')
 
 table=[]
 for line in f:
     table.append(line)
 for line in table:
-    g.write(line.split()[0]+'\t'+'1'+'\t'+str(float(line.split()[2])/float(line.split()[1]))+'\t'
-            +str(float(line.split()[3])/float(line.split()[1]))+'\t'
-            +str(float(line.split()[4])/float(line.split()[1]))+'\n')
+    g.write(line.split()[0]+'\t'+str(float(line.split()[3])/float(line.split()[1]))+'\t'
+            +str(float(line.split()[4])/float(line.split()[2]))+'\t'
+            +str(float(line.split()[5])/float(line.split()[1]))+'\t'
+            +str(float(line.split()[6])/float(line.split()[2]))+'\t'
+            +str(float(line.split()[7])/float(line.split()[1]))+'\t'
+            +str(float(line.split()[8])/float(line.split()[2]))+'\n')
+f.close()
 g.close()
